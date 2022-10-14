83d0138c3e55149de1bb6391eda337ca82ababb8
Juan Carlos <jcge97@gmail.com>
2022-10-14 19:15:46 +0100

    ShellScripting/

---
+++ ShellScripting/documentos_fasta.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Obtener los datos de .fasta, mediante wget 
+
+# Tirosina
+nano fastadata/tyrosine.txt
+
+# Lactosa
+nano fastadata/lactase.txt
+
+# Mioglobina
+nano fastadata/myoglobin.txt
--- ShellScripting/script1.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-# Obtener los datos de .fasta, mediante wget 
